2635e1ce47cb5b1e45fd1e2d317a962d2d7f8035
yan zhong <yanzhong@yandeiMac.local>
2022-01-03 20:37:53 -0500

    added autocomplete

--- screen/HomeScreen.js
+++ screen/HomeScreen.js
@@ -2,8 +2,13 @@ import React from 'react'
 import { SafeAreaView, StyleSheet, Text, View, Image } from 'react-native';
 import tw from 'twrnc';
 import NavOptions from '../components/NavOptions';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import {GOOGLE_MAPS_APIKEY} from "@env"
+import { setDestination, setOrigin } from '../slice/navSlice';
+import { useDispatch } from 'react-redux';
 
 const HomeScreen = () => {
+    const dispatch = useDispatch();
     return (
         <SafeAreaView style={tw`bg-white h-full`}>
             <View style={tw`p-5`}>
@@ -14,6 +19,34 @@ const HomeScreen = () => {
                     source={{
                         uri: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png"
                     }}
+                />
+                <GooglePlacesAutocomplete
+                    placeholder="Where From?"
+                    styles={{
+                        container: {
+                            flex:0
+                        },
+                        textInput: {
+                            fontSize: 18,
+                        },
+                    }}
+                    onPress={(data, details = null) => {
+                        dispatch(setOrigin({
+                            location: details.geometry.location,
+                            description: data.description
+                        }))
+
+                        dispatch(setDestination(null))
+                    }}
+                    fetchDetails={true}
+                    returnKeyType={"search"}
+                    enablePoweredByContainer={false}
+                    query={{
+                        key: GOOGLE_MAPS_APIKEY,
+                        language: 'en'
+                    }}
+                    nearbyPlacesAPI="GooglePlacesSearch"
+                    debounce={400}
                 />
                 <NavOptions />
             </View>
--- slice/navSlice.js
+++ slice/navSlice.js
@@ -9,7 +9,7 @@ const initialState = {
 export const navSlice = createSlice({
     name: "nav",
     initialState,
-    reducer: {
+    reducers: {
         setOrigin: (state, action) => {
             state.origin = action.paylaod;
         },
@@ -18,8 +18,8 @@ export const navSlice = createSlice({
         },
         setTravelTimeInformation: (state, action) => {
             state.travelTimeInformation = action.payload;
-        }
-    }
+        },
+    },
 })
 
 export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
