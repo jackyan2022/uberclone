ef4f90fa9e91201a5955f749029496db4c82ae73
yan zhong <yanzhong@yandeiMac.local>
2022-01-03 21:43:19 -0500

    mapview is working

---
+++ components/Map.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import MapView, {Marker} from 'react-native-maps'
+import tw from 'twrnc'
+
+const Map = () => {
+    return (
+        <MapView
+        style={tw`flex-1`}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+          
+    )
+}
+
+
+export default Map
+
+const styles = StyleSheet.create({})
--- screen/MapScreen.js
+++ screen/MapScreen.js
@@ -1,10 +1,16 @@
+import { mapSeries } from 'async';
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { NativeAppEventEmitter, StyleSheet, Text, View } from 'react-native'
+import tw from 'twrnc';
+import Map from "../components/Map";
 
 const MapScreen = () => {
     return (
         <View>
-            <Text>Here is Map</Text>
+            <View style={tw`h-1/2`}>
+                <Map />
+            </View>
+            <View style={tw`h-1/2`}></View>
         </View>
     )
 }
