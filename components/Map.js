cc29c03b3c272f5723620d3df460fbf5f5bf0f0e
yan zhong <yanzhong@yandeiMac.local>
2022-01-03 22:11:27 -0500

    fixed setOrigin

--- components/Map.js
+++ components/Map.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
 import MapView, {Marker} from 'react-native-maps'
 import tw from 'twrnc'
+import { useSelector } from 'react-redux'
+import { selectOrigin } from '../slice/navSlice'
 
 const Map = () => {
+    const origin = useSelector(selectOrigin);
     return (
         <MapView
         style={tw`flex-1`}
+        mapType="mutedStandard"
         initialRegion={{
-          latitude: 37.78825,
-          longitude: -122.4324,
-          latitudeDelta: 0.0922,
-          longitudeDelta: 0.0421,
+          latitude: origin.location.lat,
+          longitude: origin.location.lng,
+          latitudeDelta: 0.005,
+          longitudeDelta: 0.005,
         }}
       />
           
--- slice/navSlice.js
+++ slice/navSlice.js
@@ -4,26 +4,30 @@ const initialState = {
     origin: null,
     destination: null,
     travelTimeInformation: null,
-};
-
-export const navSlice = createSlice({
+  };
+  
+  export const navSlice = createSlice({
     name: "nav",
     initialState,
     reducers: {
-        setOrigin: (state, action) => {
-            state.origin = action.paylaod;
-        },
-        setDestination: (state, action) => {
-            state.destination = action.payload;
-        },
-        setTravelTimeInformation: (state, action) => {
-            state.travelTimeInformation = action.payload;
-        },
+      setOrigin: (state, action) => {
+        state.origin = action.payload;
+      },
+      setDestination: (state, action) => {
+        state.destination = action.payload;
+      },
+      setTravelTimeInformation: (state, action) => {
+        state.travelTimeInformation = action.payload;
+      },
     },
-})
-
-export const { setOrigin, setDestination, setTravelTimeInformation } = navSlice.actions;
-
-export const selectOrigin = (state) => state.nav.origin;
-export const selectDestination = (state) => state.nav.destination;
-export const selectTravelTimeInformation = (state) => state.nav.travelTimeInformation;
+  });
+  
+  export const { setOrigin, setDestination, setTravelTimeInformation } =
+    navSlice.actions;
+  
+  export const selectOrigin = (state) => state.nav.origin;
+  export const selectDestination = (state) => state.nav.destination;
+  export const selectTravelTimeInformation = (state) =>
+    state.nav.travelTimeInformation;
+  
+  export default navSlice.reducer;
