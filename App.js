44f8c03401fc342f3ce039ce55bc8b063ef73977
yan zhong <yanzhong@yandeiMac.local>
2022-01-04 22:51:52 -0500

    Added keyboardAvoid

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { KeyboardAvoidingView, Text, View, Platform } from 'react-native';
 import { Provider } from "react-redux";  
 import HomeScreen from './screen/HomeScreen';
 import MapScreen from './screen/MapScreen';
@@ -15,27 +15,24 @@ export default function App() {
     <Provider store = {store}>
       <NavigationContainer>
         <SafeAreaProvider>
-          <Stack.Navigator>
-            <Stack.Screen name='HomeScreen' component={HomeScreen} 
-              options= {{
-                headerShown: false,
-              }}/>
-              <Stack.Screen name='MapScreen' component={MapScreen} 
-              options= {{
-                headerShown: false,
-              }}/>              
-          </Stack.Navigator>
+          <KeyboardAvoidingView style={{ flex: 1}}
+            behavior={Platform.OS === "ios" ? "padding" : "height"}
+            keyboardVerticalOffset={Platform === "ios" ? -64 : 0}
+            >
+            <Stack.Navigator>
+              <Stack.Screen name='HomeScreen' component={HomeScreen} 
+                options= {{
+                  headerShown: false,
+                }}/>
+                <Stack.Screen name='MapScreen' component={MapScreen} 
+                options= {{
+                  headerShown: false,
+                }}/>              
+            </Stack.Navigator>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </NavigationContainer>
     </Provider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
